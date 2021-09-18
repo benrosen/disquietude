9eb8b7497a5061f2c1fa0fbf4857e98a7f3a8dc7
Ben Rosen <benjamin.p.rosen@gmail.com>
2021-09-18 15:56:54 -0400

    sort imports

--- src/hooks/useSeed.ts
+++ src/hooks/useSeed.ts
@@ -1,6 +1,6 @@
+import seedrandom from "seedrandom";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import seedrandom from "seedrandom";
 
 export default () => {
   const { seed } = useParams<{ seed: string }>();
